aa0fe5f1b599227f9cc4331977ae981720f668af
joao696 <joao.faust@alunos.sc.senac.br>
2024-04-04 14:56:25 -0300

    inicial

--- components/Button.js
+++ components/Button.js
@@ -5,10 +5,7 @@ export default function Button({ label, theme, onPress }) {
   if (theme === "primary") {
     return (
       <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
-        <Pressable
-          style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={onPress}
-        >
+        <Pressable style={styles.button}  onPress={onPress} >
           <FontAwesome
             name="picture-o"
             size={18}
@@ -23,7 +20,7 @@ export default function Button({ label, theme, onPress }) {
 
   return (
     <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+         <Pressable style={styles.button}  onPress={onPress} >
           <Text style={styles.buttonLabel}>{label}</Text>
         </Pressable>
       </View>
